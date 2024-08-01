b2ac0cb58af73fa550d562a14811faca44124bfd
yash yadav <115601695+theyashyadavvv@users.noreply.github.com>
2024-08-01 10:10:34 +0530

    Add files via upload
    Due to API privacy i can not able to upload my src , If anyone want the src then mail on this email yashyadev123@gmail.com

---
+++ lib/helper/global.dart
@@ -0,0 +1,13 @@
+import 'dart:ffi';
+
+import "package:flutter/material.dart";
+
+//app name
+const appName = 'Ai Assistant';
+
+//media query to store size of device screen
+late Size mq;
+
+//chat gpt api key (Updated it)
+// String apiKey = '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';
+String apiKey = '';
